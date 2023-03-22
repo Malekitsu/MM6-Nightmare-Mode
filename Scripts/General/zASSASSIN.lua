69241bf66108e6185fe3a1e9743f60b91962caa7
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2023-03-22 12:00:38 +0100

    Add files via upload

--- Scripts/General/zASSASSIN.lua
+++ Scripts/General/zASSASSIN.lua
@@ -1,3 +1,11 @@
+local Rebalance
+Rebalance = 1
+Rebalanze = 1
+if SETTINGS["ImbaSubClasses"]==false then
+Rebalance = 0.95
+Rebalanze = 0
+end
+
 ASSASSIN=SETTINGS["ArcherAsAssassin"]
 if ASSASSIN==true then
 
@@ -8,7 +16,7 @@ function events.CalcDamageToMonster(t)
 	if data.Player and (data.Player.Class==const.Class.WarriorMage or data.Player.Class==const.Class.BattleMage or data.Player.Class==const.Class.Archer) and t.DamageKind==0 and data.Object==nil then
 			speed=data.Player:GetSpeed()
 			bonusDamage=speed/500
-			t.Result=t.Result*(1+bonusDamage)
+			t.Result=t.Result*(1+bonusDamage*Rebalanze)*Rebalance
 		end
 	
 end
@@ -52,8 +60,6 @@ function events.CalcSpellDamage(t)
 --get mastery
 
 	local data = WhoHitMonster()
-
-
 	if data.Player and (data.Player.Class==const.Class.WarriorMage or data.Player.Class==const.Class.BattleMage or data.Player.Class==const.Class.Archer) then	
 	
 	mastery=data.Player.Skills[const.Skills.Thievery]
@@ -145,11 +151,72 @@ function events.GameInitialized2()
 	Game.ClassKinds.StartingSkills[4][const.Skills.Chain] = 0
 	Game.ClassKinds.StartingSkills[4][const.Skills.Bodybuilding] = 2
 	Game.ClassKinds.StartingSkills[4][const.Skills.Thievery] = 1
+	Game.ClassKinds.StartingSkills[4][const.Skills.Meditation] = 0
 	Game.ClassKinds.SPBase[4] = 100
 Game.ClassNames[const.Class.Archer]="Rogue"
 Game.ClassNames[const.Class.BattleMage]="Shadow"
 Game.ClassNames[const.Class.WarriorMage]="Assassin"
-Game.ClassDescriptions[const.Class.Archer] = "A rogue is a daring and cunning warrior, skilled in the art of deception and quick thinking. Their agility and speed allow them to strike with deadly precision, always staying one step ahead of their opponents.\n\nStats:\nHP: 2-3-4 per level (depending on promotion)\nEnergy (SP): 100 base, no SP per level\n5% dodge\n\nAbilities:\nCan use Elemental Spells\nEach 5 points in Speed will increase damage by 1%\nMelee attacks have a 30% chance to restore 15 energy.\nYou will automatically restore 40 energy every 10 seconds\nMastery will add 1% dodge and 5% combo point skills\n\nAttack:\nConsumes 30 energy to deal 20%+10% per mastery increased damage (can crit)\nAdds a combo point, up to 5\n\nSpell Cast:\nConsumes all combo points\nDeals 20% increased damage per combo point consumed\nIf 5 combo points are consumed, damage will increase by 250%\nSpell cast with no combo points will do half the damage\n\nEnergy Increase:\nSP increasing item will instead increase MAX energy by 20% of the effect. " 
-
+Game.ClassDescriptions[const.Class.Archer] = "A rogue is a daring and cunning warrior, skilled in the art of deception and quick thinking. Their agility and speed allow them to strike with deadly precision, always staying one step ahead of their opponents.\n\nStats:\nHP: 2 per level\nEnergy (SP): 100 base, no SP per level\n5% dodge\n\nAbilities:\nCan use Elemental Spells\nEach 5 points in Speed will increase damage by 1%\nMelee attacks have a 30% chance to restore 15 energy.\nYou will automatically restore 40 energy every 10 seconds\nMastery will add 1% dodge and 5% combo point skills\n\nAttack:\nConsumes 30 energy to deal 20%+10% per mastery point increased damage (can crit)\nAdds a combo point, up to 5\n\nSpell Cast:\nConsumes all combo points\nDeals 20% increased damage per combo point consumed\nIf 5 combo points are consumed, damage will increase by 250%\nSpell cast with no combo points will do half the damage\n\nMastery\nEach point of mastery will increase damage of energy spender by 10%, and combo point ability by 5% " 
+Game.ClassDescriptions[const.Class.BattleMage] ="Shadow, the ultimate evolution of the rogue. Masters of deadly precision, they strike with unmatched agility and cunning. Their blades cut through enemies with ease, and their power lies in their ability to manipulate darkness. They are the masters of fear, striking terror into the hearts of their foes. The Shadow is a force to be reckoned with, feared by all who stand in their way.\nStats:\nHP: 3 per level\nEnergy (SP): 100 base, no SP per level\n5% dodge\nAbilities:\nCan use Elemental Spells\nEach 5 points in Speed will increase damage by 1%\nMelee attacks have a 30% chance to restore 15 energy.\nYou will automatically restore 40 energy every 10 seconds\nMastery will add 1% dodge and 5% combo point skills\n\nAttack:\nConsumes 30 energy to deal 20%+10% per mastery point increased damage (can crit)\nAdds a combo point, up to 5\n\nSpell Cast:\nConsumes all combo points\nDeals 20% increased damage per combo point consumed\nIf 5 combo points are consumed, damage will increase by 250%\nSpell cast with no combo points will do half the damage\n\nMastery\nEach point of mastery will increase damage of energy spender by 10%, and combo point ability by 5% " 
+Game.ClassDescriptions[const.Class.WarriorMage] ="Assassin, the ultimate evolution of the shadow. They strike with ruthless precision and deadly blades, able to eliminate their targets with a single, fatal blow. Their movements are graceful and silent, and their mastery of assassination is unmatched. \n\nStats:\nHP: 4 per level\nEnergy (SP): 100 base, no SP per level\n5% dodge\n\nAbilities:\nCan use Elemental Spells\nEach 5 points in Speed will increase damage by 1%\nMelee attacks have a 30% chance to restore 15 energy.\nYou will automatically restore 40 energy every 10 seconds\nMastery will add 1% dodge and 5% combo point skills\n\nAttack:\nConsumes 30 energy to deal 20%+10% per mastery point increased damage (can crit)\nAdds a combo point, up to 5\n\nSpell Cast:\nConsumes all combo points\nDeals 20% increased damage per combo point consumed\nIf 5 combo points are consumed, damage will increase by 250%\nSpell cast with no combo points will do half the damage\n\nMastery\nEach point of mastery will increase damage of energy spender by 10%, and combo point ability by 5% "
 end	
+
+--timers
+local function restoreManaArcher() 
+	for _, pl in Party do 
+		if pl.Class == const.Class.Archer then 
+			pl.SP = math.min(pl.SP + 1, pl:GetFullSP()) 
+		end 
+	end 
 end
+--Timer(restoreManaArcher, const.Second*30) 
+
+local function restoreManaBattleMage() 
+	for _, pl in Party do 
+		if pl.Class == const.Class.BattleMage then 
+			pl.SP = math.min(pl.SP + 1, pl:GetFullSP()) 
+		end 
+	end 
+end
+--Timer(restoreManaBattleMage, const.Second*20) 
+
+local function restoreManaWarriorMage() 
+	for _, pl in Party do 
+		if pl.Class == const.Class.WarriorMage then 
+			pl.SP = math.min(pl.SP + 1, pl:GetFullSP()) 
+		end 
+	end 
+end
+--Timer(restoreManaWarriorMage, const.Second*15) 
+
+function events.LoadMap(wasInGame)
+local function restoreManaArcher() 
+	for _, pl in Party do 
+		if pl.Class == const.Class.Archer then 
+			pl.SP = math.min(pl.SP + 2, pl:GetFullSP()) 
+		end 
+	end 
+end
+Timer(restoreManaArcher, const.Minute/2) 
+
+local function restoreManaBattleMage() 
+	for _, pl in Party do 
+		if pl.Class == const.Class.BattleMage then 
+			pl.SP = math.min(pl.SP + 3, pl:GetFullSP()) 
+		end 
+	end 
+end
+Timer(restoreManaBattleMage, const.Minute/2) 
+
+local function restoreManaWarriorMage() 
+	for _, pl in Party do 
+		if pl.Class == const.Class.WarriorMage then 
+			pl.SP = math.min(pl.SP + 4, pl:GetFullSP()) 
+		end 
+	end 
+end
+Timer(restoreManaWarriorMage, const.Minute/2) 
+end
+
+end
+
--- Scripts/General/zSERAPHIN.lua
+++ Scripts/General/zSERAPHIN.lua
@@ -1,3 +1,12 @@
+local Rebalance
+Rebalance = 1
+Rebalanze = 1
+if SETTINGS["ImbaSubClasses"]==false then
+Rebalance = 0.95
+Rebalanze = 0
+end
+
+
 SERAPHIN=SETTINGS["ClericAsSeraphin"]
 if SERAPHIN==true then
 
@@ -127,13 +136,13 @@ function events.CalcDamageToMonster(t)
 			might=data.Player:GetMight()
 			personality=data.Player:GetPersonality()
 			bonusDamage=might/1000 + personality/1000
-			t.Result=t.Result*(1+bonusDamage)
+			t.Result=t.Result*(1+bonusDamage*Rebalanze)*Rebalance
 		end
 end
 
 --AUTORESS SKILL
 function events.CalcDamageToPlayer(t)
-	if (t.Player.Class==const.Class.HighPriest or t.Player.Class==const.Class.Priest or t.Player.Class==const.Class.Cleric) and t.Player.Unconscious==0 and t.Player.Dead==0 and t.Player.Eradicated==0  and ressed~=1 then
+	if (t.Player.Class==const.Class.HighPriest or t.Player.Class==const.Class.Priest or t.Player.Class==const.Class.Cleric) and t.Player.Unconscious==0 and t.Player.Dead==0 and t.Player.Eradicated==0 then
 		if t.Result>=t.Player.HP then
 			totMana=t.Player:GetFullSP()
 			currentMana=t.Player.SP
@@ -150,14 +159,77 @@ function events.CalcDamageToPlayer(t)
 				end
 				t.Player.HP=t.Player.HP+(totMana/4)*(1+mastery*0.05)
 			
---			ressed=1
---			Sleep(16000)
-			ressed=0
+
 
 			end
 		end	
 	end
 end
+
+-- DIVINE SHIELD 
+--tables
+function events.LoadMap(wasInGame)
+vars.divineShieldTime=vars.divineShieldTime or {}
+vars.divineShieldCooldown=vars.divineShieldCooldown or {}
+vars.divineShield=vars.divineShield or {}
+end
+--end table
+function events.CalcDamageToPlayer(t)
+	if (t.Player.Class==const.Class.HighPriest or t.Player.Class==const.Class.Priest or t.Player.Class==const.Class.Cleric) and t.Player.Unconscious==0 and t.Player.Dead==0 and t.Player.Eradicated==0  and vars.divineShield[t.PlayerIndex]==1 then
+	t.Result=0
+	Game.ShowStatusText("Immune")
+	end
+end
+--activation button	
+function Keys.E(t)
+CPI=Game.CurrentPlayer
+	if CPI==-1 then
+	return
+	end
+if Party[CPI].Class==const.Class.HighPriest or Party[CPI].Class==const.Class.Priest or Party[CPI].Class==const.Class.Cleric then
+	if vars.divineShield[CPI]==1 then
+	Game.ShowStatusText(string.format("Divine Shield is already Active, expiring in %s seconds. ",math.max(math.floor((vars.divineShieldTime[CPI] - Game.Time) / 128 * 10) / 10,0)))
+		else if vars.divineShieldCooldown[CPI] ~= nil and vars.divineShieldCooldown[CPI] > Game.Time then
+		Game.ShowStatusText(string.format("Divine Shield is on cooldown. Try again in %s seconds.",math.floor((vars.divineShieldCooldown[CPI] - Game.Time) / 128 * 10) / 10))
+			else
+				cost=Party[CPI].LevelBase+5
+				if Party[CPI].SP>cost then
+				Party[CPI].SP=Party[CPI].SP-cost
+				Game.ShowStatusText(string.format("%s activates 'Divine Shield'",Party[CPI].Name))
+				vars.divineShield[CPI] = 1
+				vars.divineShieldTime[CPI] = Game.Time + const.Minute * 6
+				vars.divineShieldCooldown[CPI] = Game.Time + const.Minute * 90
+				else 
+				Game.ShowStatusText(string.format("Not enought mana (%s mana needed)",cost))
+				end
+			end
+		end
+	end
+end
+--
+function events.LoadMap(wasInGame)
+local function divineShieldTimer() 
+	for i = 0, 3 do
+		if vars.divineShieldTime[i]~=nil and vars.divineShield[i]~=0 then
+			if vars.divineShieldTime[i]<Game.Time then
+			vars.divineShield[i]=0
+			Game.ShowStatusText(string.format("%s's Divine Shield effect expired",Party[i].Name))
+			end 
+		end
+	end
+end
+Timer(divineShieldTimer, const.Minute/2) 
+end
+--reduced damage when in divine shield
+function events.CalcDamageToMonster(t)
+	 data=WhoHitMonster()
+	 ind=data.Player:GetIndex()
+		if data.Player and (data.Player.Class==const.Class.HighPriest or data.Player.Class==const.Class.Priest or data.Player.Class==const.Class.Cleric) and vars.divineShield[ind]==1 then
+			t.Result=t.Result/2
+		end
+end
+
+-------------end of divine shield---------------
 
 
 
@@ -185,8 +257,9 @@ Game.ClassKinds.StartingSkills[1][const.Skills.Dark] = 0
 Game.ClassNames[const.Class.Cleric]="Seraphin"
 Game.ClassNames[const.Class.Priest]="Angel"
 Game.ClassNames[const.Class.HighPriest]="Archangel"
-Game.ClassDescriptions[const.Class.Cleric] = "Seraphin is a divine warrior, blessed by the gods with otherworldly powers that set him apart from mortal fighters. His origins are shrouded in mystery, but it is said that he was chosen by the divine to carry out their will on the mortal plane. Some whisper that he was born from the union of a mortal and an angel, while others believe that he was created by the gods themselves. Regardless of his origins, there is no denying the power that Seraphin wields, and his presence on the battlefield is a testament to the will of the divine.\n\nStats:\n+20 starting HP and +10 mana points\n\nProficiency in Plate, Sword, Mace, and Shield (offhand must be disabled)\n3-4-5 HP and 2-3-4 mana points\ngained per level\nDamage scaling based on Might and Personality (each 10 points adds 1% damage)\n\nAbilities:\nDivine Attacks that deal extra damage based on Light skill (2-3-4 damage added per point in Light at Novice/Expert/Master, increased by 10% per mastery point)\nAttacking will heal the most injured party member based on Body skill (2-2.5-3 points per point in Body at Novice/Expert/Master, increased by 5% per mastery point)\nDivine Protection that converts up to 25% of mana into self-healing when facing lethal attacks (increased by 5% per mastery point)."
-
+Game.ClassDescriptions[const.Class.Cleric] = "Seraphin is a divine warrior, blessed by the gods with otherworldly powers that set him apart from mortal fighters. His origins are shrouded in mystery, but it is said that he was chosen by the divine to carry out their will on the mortal plane. Some whisper that he was born from the union of a mortal and an angel, while others believe that he was created by the gods themselves. Regardless of his origins, there is no denying the power that Seraphin wields, and his presence on the battlefield is a testament to the will of the divine.\n\nStats:\n+20 starting HP and +10 mana points\nProficiency in Plate, Sword, Mace, and Shield (offhand must be disabled)\n3 HP and 2 mana points gained per level\nDamage scaling based on Might and Personality (each 10 points adds 1% damage)\n\nAbilities:\n\nGods Wrath:Attacks deal extra damage based on Light skill (2-3-4 damage added per point in Light at Novice/Expert/Master, increased by 10% per mastery point)\n\nHoly Strikes: Attacking will heal the most injured party member based on Body skill (2-2.5-3 points per point in Body at Novice/Expert/Master, increased by 5% per mastery point)\n\nDivine Protection: converts up to 25% of mana into self-healing when facing lethal attacks (increased by 5% per mastery point).\n\nDivine Shield: Grants the caster invincibility for 12 seconds, rendering them impervious to all forms of harm. This ability has a cooldown of 180 seconds."
+Game.ClassDescriptions[const.Class.Priest] = "The Angel, the ultimate form of seraphic evolution. Radiant and powerful, they wield holy magic with grace, and their wings span great distances as they soar through the heavens. Their mere presence fills mortals with awe and courage, and they are protectors of the weak and defenders of the just. Truly, the Angel is a magnificent being, a testament to the glory of the divine.\n\nStats:\nProficiency in Plate, Sword, Mace, and Shield (offhand must be disabled)\n4 HP and 3 mana points gained per level\nDamage scaling based on Might and Personality (each 10 points adds 1% damage)\n\nAbilities:\n\nGods Wrath:Attacks deal extra damage based on Light skill (2-3-4 damage added per point in Light at Novice/Expert/Master, increased by 10% per mastery point)\n\nHoly Strikes: Attacking will heal the most injured party member based on Body skill (2-2.5-3 points per point in Body at Novice/Expert/Master, increased by 5% per mastery point)\n\nDivine Protection: converts up to 25% of mana into self-healing when facing lethal attacks (increased by 5% per mastery point).\n\nDivine Shield: Grants the caster invincibility for 12 seconds, rendering them impervious to all forms of harm. This ability has a cooldown of 180 seconds."
+Game.ClassDescriptions[const.Class.Priest] = "The Archangel, the pinnacle of angelic evolution. Radiant and powerful, their wings shine like the sun, and their divine presence inspires awe and reverence. They wield holy magic with effortless skill, their swords and shields imbued with the force of the cosmos. As the guardians of the divine realm, their judgment is swift and true, their mercy boundless. The Archangel is the embodiment of divine justice and the ultimate manifestation of angelic might.\n\nStats:\nProficiency in Plate, Sword, Mace, and Shield (offhand must be disabled)\n5 HP and 4 mana points gained per level\nDamage scaling based on Might and Personality (each 10 points adds 1% damage)\n\nAbilities:\n\nGods Wrath:Attacks deal extra damage based on Light skill (2-3-4 damage added per point in Light at Novice/Expert/Master, increased by 10% per mastery point)\n\nHoly Strikes: Attacking will heal the most injured party member based on Body skill (2-2.5-3 points per point in Body at Novice/Expert/Master, increased by 5% per mastery point)\n\nDivine Protection: converts up to 25% of mana into self-healing when facing lethal attacks (increased by 5% per mastery point).\n\nDivine Shield: Grants the caster invincibility for 12 seconds, rendering them impervious to all forms of harm. This ability has a cooldown of 180 seconds."
 end
 
 end
