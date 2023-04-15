332513861810c026b9940cd517e36d0d8234c1ea
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2023-04-16 01:27:34 +0200

    Update skill-mod.lua

--- Scripts/General/skill-mod.lua
+++ Scripts/General/skill-mod.lua
@@ -2035,11 +2035,11 @@ local function BBHook(amountReg)
 		
 		enduranceBonus=pl:GetEndurance()/500
 		if s == 0 then	
-		if SETTINGS["StatsRework"]==true then
-		d[amountReg] = d[amountReg] * (1+enduranceBonus)
-		end
+			if SETTINGS["StatsRework"]==true then
+			d[amountReg] = d[amountReg] * (1+enduranceBonus)
+			end
 		return end
-		d[amountReg] = amount + math.round(amount * 0.01 * s)		
+		d[amountReg] = amount + s^2		
 		if SETTINGS["StatsRework"]==true then
 		d[amountReg] = d[amountReg] * (1+enduranceBonus)
 		end
@@ -2053,6 +2053,7 @@ mem.autohook(0x47FD71, BBHook("edi"))
 -- Rest()
 mem.autohook(0x484FF3, BBHook("edi"))
 -- HP regeneration
+mem.autohook(0x487F05, BBHook("edi"))
 mem.autohook(0x487D8A, BBHook("edi"))
 
 -- allow to hold sword in left hand at novice rank
