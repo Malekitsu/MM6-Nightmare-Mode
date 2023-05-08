8498109b686bb993aa29180993a3a77b1ed444a2
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2023-05-09 01:15:27 +0200

    Update skem-spell-overrides.lua

--- Scripts/General/skem-spell-overrides.lua
+++ Scripts/General/skem-spell-overrides.lua
@@ -860,6 +860,8 @@ end
 mem.hookcall(0x0047FF37, 1, 1, changedCharacterCalcStatBonusByItems)
 mem.hookcall(0x0048875B, 1, 1, changedCharacterCalcStatBonusByItems)
 
+local ADAPTIVE = string.lower(SETTINGS["AdaptiveMonsterMode"])
+
 -- modified Monster Calculate Damage
 -- supersedes skill-mod.lua:2658-2693
 local function modifiedMonsterCalculateDamage(d, def, monsterPointer, attackType)
@@ -867,7 +869,13 @@ local function modifiedMonsterCalculateDamage(d, def, monsterPointer, attackType
 	-- get monster
 
 	local monsterIndex, monster = GetMonster(d.edi)
+if ((ADAPTIVE == "default") or (ADAPTIVE == "disabled")) then 
 	Mlevel = Game.MonstersTxt[monster.Id].Level
+else
+Xlevel = Game.MonstersTxt[monster.Id].Level
+Mlevel = monsterArray["Level"]
+Mlevel = monsterArray["Level"] * math.max(1, Mlevel/Xlevel)
+end
 	-- execute original code
 
 	local damage = def(monsterPointer, attackType)
@@ -889,8 +897,12 @@ local function modifiedMonsterCalculateDamage(d, def, monsterPointer, attackType
 	-- calculate spell damage same way as for party
 
 	local spellSkill, spellMastery = SplitSkill(monster.SpellSkill)
-	damage = Game.CalcSpellDamage(monster.Spell, spellSkill, spellMastery, 0) * DifficultyModifier 
-
+	if SETTINGS["ItemRework"]==true and SETTINGS["StatsRework"]==true then
+	damage = Game.CalcSpellDamage(monster.Spell, spellSkill, spellMastery, 0) * DifficultyModifier * ((Mlevel/30)+0.75) * (Mlevel^1.5/1000+1)
+	else
+	damage = Game.CalcSpellDamage(monster.Spell, spellSkill, spellMastery, 0) * DifficultyModifier * ((Mlevel/30)+0.75)
+	end
+	
 	return damage
 
 end
@@ -1177,7 +1189,12 @@ local function modifiedHealCharacterWithSpell(d, def, targetPtr, amount)
 	t.TargetIndex, t.Target = GetPlayer(targetPtr) -- also index u2[spellStructurePtr + 4]
 	t.Spell = mem.u2[spellStructurePtr]
 	t.CasterIndex, t.Caster = mem.u2[spellStructurePtr + 2], Party.PlayersArray[mem.u2[spellStructurePtr + 2] ]
-	t.Skill, t.Mastery = SplitSkill(t.Caster.Skills[const.Skills.Fire + math.ceil(t.Spell / 11) - 1])
+	if mem.u1[spellStructurePtr + 8]:And(1) ~= 0 then -- casted by scroll?
+		t.Skill, t.Mastery = SplitSkill(mem.u1[spellStructurePtr + 10])
+		t.FromScroll = true
+	else
+		t.Skill, t.Mastery = SplitSkill(t.Caster.Skills[const.Skills.Fire + math.ceil(t.Spell / 11) - 1])
+	end
 	events.call("HealingSpellPower", t)
 	def(targetPtr, t.Result)
 end
@@ -1230,7 +1247,12 @@ mem.hookfunction(0x484840, 1, 3, function(d, def, playerPtr, cond, timeLow,  tim
 	local t = {Condition = cond, Spell = mem.u2[d.ebx]}
 	t.CasterIndex, t.Caster = mem.u2[d.ebx + 2], Party.PlayersArray[mem.u2[d.ebx + 2] ]
 	t.TargetIndex, t.Target = GetPlayer(playerPtr)
-	t.Skill, t.Mastery = SplitSkill(t.Caster.Skills[const.Skills.Fire + math.ceil(t.Spell / 11) - 1])
+	if mem.u1[d.ebx + 8]:And(1) ~= 0 then -- casted by scroll?
+		t.Skill, t.Mastery = SplitSkill(mem.u1[d.ebx + 10])
+		t.FromScroll = true
+	else
+		t.Skill, t.Mastery = SplitSkill(t.Caster.Skills[const.Skills.Fire + math.ceil(t.Spell / 11) - 1])
+	end
 	-- time is calculated by subtracting spell's time limit from Game.Time, and then
 	-- checking if result is <= condition affect time,
 	-- so to calc spell time limit we need to subtract time from Game.Time
