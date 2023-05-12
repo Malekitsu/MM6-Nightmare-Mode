5c5c06576cf8d480ac6b73cf43554fb48773ed56
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2023-05-12 21:47:47 +0200

    temple of baa is a bit more accessible

--- Scripts/Maps/outd3.lua
+++ Scripts/Maps/outd3.lua
@@ -6,32 +6,32 @@ Game.MapEvtLines:RemoveEvent(94)
 
 evt.house[94] = 191  -- "Temple of Baa"
 evt.map[94] = function()
+enter=0
 	if evt.Cmp("Awards", 8) or evt.Cmp("Awards", 9) then
-		if evt.Cmp("Awards", 12) or evt.Cmp("Awards", 13) then
-			if evt.Cmp("Awards", 16) or evt.Cmp("Awards", 17) then
-				if evt.Cmp("Awards", 20) or evt.Cmp("Awards", 21) then
-					if evt.Cmp("Awards", 24) or evt.Cmp("Awards", 25) then
-						if evt.Cmp("Awards", 28) or evt.Cmp("Awards", 29) then
-							evt.MoveToMap{X = -15592, Y = 120, Z = -191, Direction = 0, LookAngle = 0, SpeedZ = 0, HouseId = 191, Icon = 5, Name = "T1.Blv"}         -- "Temple of Baa"					
-						else
-						Message("You need all the promotions to enter here")
-						end	
-					else
-					Message("You need all the promotions to enter here")
-					end
-				else
-				Message("You need all the promotions to enter here")
-				end
-			else
-			Message("You need all the promotions to enter here")
-			end	
-		else
-		Message("You need all the promotions to enter here")
-		end			
-	else
-	Message("You need all the promotions to enter here")
+		enter=enter+1
+	end	
+	if evt.Cmp("Awards", 12) or evt.Cmp("Awards", 13) then
+		enter=enter+1
+	end		
+	if evt.Cmp("Awards", 16) or evt.Cmp("Awards", 17) then
+	enter=enter+1
 	end
+	if evt.Cmp("Awards", 20) or evt.Cmp("Awards", 21) then
+	enter=enter+1
+	end
+	if evt.Cmp("Awards", 24) or evt.Cmp("Awards", 25) then
+	enter=enter+1
+	end
+	if evt.Cmp("Awards", 28) or evt.Cmp("Awards", 29) then
+	enter=enter+1
+	end
+	if enter>=4 then
+		evt.MoveToMap{X = -15592, Y = 120, Z = -191, Direction = 0, LookAngle = 0, SpeedZ = 0, HouseId = 191, Icon = 5, Name = "T1.Blv"}         -- "Temple of Baa"		
+	else
+		Message("YOU ARE NOT PREPARED\n(You need at least 4 promotions to enter here)")
+	end	
 end
+
 
 evt.hint[333] = "strange Statuette"
 evt.map[333] = function()
