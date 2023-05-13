2e2fd841ea91b5f4bcd59f746dd93c7ad8a9c5b3
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2023-05-13 13:27:47 +0200

    Update zITEMSREWORK.lua

--- Scripts/General/zITEMSREWORK.lua
+++ Scripts/General/zITEMSREWORK.lua
@@ -544,9 +544,10 @@ local function restoreHPEnchant()
 	totHP=pl:GetFullHP()
 		for it in pl:EnumActiveItems() do
 			if it.Bonus2 == 37 or it.Bonus2==44 or it.Bonus2==50 or it.Bonus2==54 then		
-				HPregen=HPregen+totHP*0.005	
+				HPregen=math.max(HPregen+totHP*0.005,HPregen+1)	
 			end
 		end
+	HPregen=math.max(HPregen-1,0)
 	pl.HP=math.min(pl.HP+math.round(HPregen),totHP)
 	end 
 end
@@ -559,9 +560,10 @@ local function restoreSPEnchant()
 	totSP=pl:GetFullSP()
 		for it in pl:EnumActiveItems() do
 			if it.Bonus2 == 38 or it.Bonus2==47 or it.Bonus2==55 then		
-				SPregen=SPregen+totSP*0.005
+				SPregen=math.max(SPregen+totSP*0.005,SPregen+1)
 			end
 		end
+	SPregen=math.max(SPregen-1,0)
 	pl.SP=math.min(pl.SP+math.round(SPregen),totSP)
 	end 
 end
