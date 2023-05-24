a4d37be30fbde6034e664dd896933929a36500fa
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2023-05-25 01:55:29 +0200

    Update zzzOverFlow.lua

--- Scripts/General/zzzOverFlow.lua
+++ Scripts/General/zzzOverFlow.lua
@@ -1,90 +1,32 @@
+PowerCureOverflow=SETTINGS["PowerCureOverflow"]
 function events.HealingSpellPower(t)
-if t.Spell == 54 then
+  if t.Spell == 54 then
+    active = 0
+    for i = 0, 3 do
+      if Party[i].Dead == 0 and Party[i].Eradicated == 0 and Party[i].Stoned == 0 and Party[i].Unconscious == 0 then
+        active = active + 1
+      end
+    end
 
-healHP = (Party[0].HP + Party[1].HP + Party[2].HP + Party[3].HP + t.Skill * 9)/4
-		a = Party[0]:GetFullHP()-Party[0].HP+healHP
-	if Party[0].Dead~=1 and Party[0].Eradicated~=1 then
-	
-		if Party[0].HP < 0 then
-		a2 = 0
-		a3 = 1
-		elseif healHP > Party[0]:GetFullHP() then
-		a2 = healHP - Party[0]:GetFullHP()
-		a3 = 1
-		elseif Party[0].HP > Party[0]:GetFullHP()*0.9 then
-		a2 = 0
-		a3 = 1
+    SETTEDHP = t.Result / active
+    overflow = 0
+	needheal = 0
+    for i = 0, 3 do
+      if Party[i].Dead == 0 and Party[i].Eradicated == 0 and Party[i].Stoned == 0 and Party[i].Unconscious == 0 then
+        if SETTEDHP > Party[i]:GetFullHP() then
+          overflow = overflow + (SETTEDHP - Party[i]:GetFullHP())
 		else
-		a2 = 0
-		a3 = 1
-		end
-		end
-
-		b = Party[1]:GetFullHP()-Party[1].HP+healHP
-	if Party[1].Dead~=1 and Party[1].Eradicated~=1 then
-		b2 = 0
-		b3 = 0
-		if Party[1].HP < 0 then
-		b2 = 0
-		b3 = 0
-		elseif healHP > Party[1]:GetFullHP() then
-		b2 = healHP - Party[1]:GetFullHP()
-		b3 = 1
-		elseif Party[1].HP > Party[1]:GetFullHP()*0.9 then
-		a2 = 0
-		a3 = 1
-		else
-		b2 = 0
-		b3 = 0
-
-		end
-		end
-
-		c = Party[2]:GetFullHP()-Party[2].HP+healHP
-	if Party[2].Dead~=1 and Party[2].Eradicated~=1 then
-		c2 = 0
-		c3 = 0
-		if Party[2].HP < 0 then
-		c2 = 0
-		c3 = 0
-		elseif healHP > Party[2]:GetFullHP() then
-		c2 = healHP - Party[2]:GetFullHP()
-		c3 = 1
-		elseif Party[2].HP > Party[2]:GetFullHP()*0.9 then
-		a2 = 0
-		a3 = 1
-		else
-		c2 = 0
-		c3 = 0
-
-		end
-		end
-
-		d = Party[3]:GetFullHP()-Party[3].HP+healHP
-	if Party[3].Dead~=1 and Party[3].Eradicated~=1 then
-		d2 = 0
-		d3 = 0
-		if Party[3].HP < 0 then
-		d2 = 0
-		d3 = 0
-		elseif healHP > Party[3]:GetFullHP() then
-		d2 = healHP - Party[3]:GetFullHP()
-		d3 = 1
-		elseif Party[3].HP > Party[3]:GetFullHP()*0.9 then
-		a2 = 0
-		a3 = 1
-		else
-		d2 = 0
-		d3 = 0
-
-		end
-		end
-
-		surplus = a2+b2+c2+d2
-		Play = a3+b3+c3+d3
-t.Result = math.min(t.Result+surplus*4/(4.5-Play), 32767)
+		needheal=needheal+1
+        end
+      end
+    end
+	if needheal>0 and overflow>0 then
+	t.Result=t.Result+overflow*4/needheal
+	end
 end
-if t.Spell == 77 then
+
+
+if t.Spell == 77 and PowerCureOverflow then
 --t.Result = t.Result / 4
 a2=0
 b2=0
@@ -126,13 +68,13 @@ d2=0
 
 MissHP = a + b + c + d
 surplus = a2 + b2 + c2 + d2
+surplus = surplus/4
 
 
-
-Party[0].HP = math.min(Party[0]:GetFullHP(), Party[0].HP + surplus * a / MissHP, 32767)
-Party[1].HP = math.min(Party[1]:GetFullHP(), Party[1].HP + surplus * b / MissHP, 32767)
-Party[2].HP = math.min(Party[2]:GetFullHP(), Party[2].HP + surplus * c / MissHP, 32767)
-Party[3].HP = math.min(Party[3]:GetFullHP(), Party[3].HP + surplus * d / MissHP, 32767)
+Party[0].HP = math.min(Party[0]:GetFullHP(), Party[0].HP + surplus * a / MissHP)
+Party[1].HP = math.min(Party[1]:GetFullHP(), Party[1].HP + surplus * b / MissHP)
+Party[2].HP = math.min(Party[2]:GetFullHP(), Party[2].HP + surplus * c / MissHP)
+Party[3].HP = math.min(Party[3]:GetFullHP(), Party[3].HP + surplus * d / MissHP)
 if Party[0].HP > Party[0]:GetFullHP() then
 Party[0].HP = Party[0]:GetFullHP()
 end
