ee918872b535235d3b406a6fd5437a7cdbd3452c
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2023-06-11 22:10:57 +0200

    Add files via upload

---
+++ Scripts/Structs/editableStatDescriptions.lua
@@ -0,0 +1,22 @@
+local oldGame = structs.f.GameStructure
+function structs.f.GameStructure(define, ...)
+    oldGame(define, ...)
+    local cs = const.Stats
+    define[0x56B7E8].union("ExtraStatDescriptions")
+    .EditPChar(cs.HP)
+    .EditPChar(cs.SP)
+    .EditPChar(cs.ArmorClass)
+    .skip(12)
+    .EditPChar(cs.Level)
+    .skip(4)
+    .EditPChar(cs.MeleeAttack)
+    .EditPChar(cs.MeleeDamageBase)
+    .EditPChar(cs.RangedAttack)
+    .EditPChar(cs.RangedDamageBase)
+    .EditPChar(cs.FireResistance)
+    .EditPChar(cs.ElecResistance)
+    .EditPChar(cs.ColdResistance)
+    .EditPChar(cs.PoisonResistance)
+    .EditPChar(cs.MagicResistance)
+    .union()
+end
