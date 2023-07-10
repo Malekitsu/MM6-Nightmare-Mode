1176e5aee72291f61ec291389f006621a5fb3665
Eksekk <eksekk3@gmail.com>
2023-07-10 19:00:51 +0200

    Fix repairing items

--- Scripts/Structs/extraDataInItemStruct.lua
+++ Scripts/Structs/extraDataInItemStruct.lua
@@ -3,3 +3,9 @@ function structs.f.Item(define, ...)
     oldItem(define, ...)
     define[0x16].i2("ExtraData")
 end
+
+-- fix repairing items zeroing extra data
+mem.asmpatch(0x41C71C ,[[
+    and byte [ebx + 0x14], 0xFD ; item repaired
+    or byte [ebx + 0x14], 1 ; item identified
+]])
