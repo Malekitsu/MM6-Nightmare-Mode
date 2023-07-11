bc3ff70c5d895a74b9cf5de82b055f1b2a624ab9
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2023-07-11 17:25:29 +0200

    Update SCI-FI.lua

--- Scripts/Maps/SCI-FI.lua
+++ Scripts/Maps/SCI-FI.lua
@@ -652,7 +652,7 @@ if evt.Cmp("Awards", 61) then
 	--remove 1st promotions
 		evt.SetNPCTopic{NPC = 6, Index = 1, Event = 70}   
 		evt.SetNPCTopic{NPC = 5, Index = 1, Event = 59} 
-		evt.SetNPCTopic{NPC = 4, Index = 1, Event = 16} 
+		evt.SetNPCTopic{NPC = 4, Index = 1, Event = 13} 
 		evt.SetNPCTopic{NPC = 16, Index = 1, Event = 37} 
 		evt.SetNPCTopic{NPC = 15, Index = 1, Event = 93}
 		evt.SetNPCTopic{NPC = 14, Index = 1, Event = 84}  
