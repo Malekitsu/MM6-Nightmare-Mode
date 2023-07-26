a29a9f410cfadaa600ced7b1c9c14360e0c13a2e
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2023-07-27 01:32:33 +0200

    Update GLOBAL.lua

--- Scripts/Global/GLOBAL.lua
+++ Scripts/Global/GLOBAL.lua
@@ -3766,3 +3766,32 @@ evt.global[457] = function()
 	end
 end
 
+--credits to Grayface for the following code code ;)
+if SETTINGS["TRUENIGHTMARE"]==true then
+	local function NeedSeed()
+		local t = mapvars.MonsterSeed
+		if not t then
+			t = {}
+			mapvars.MonsterSeed = t
+			for i = 0, Map.Monsters.Limit - 1 do
+				t[i] = Game.RandSeed
+				for i = 1, 30 do
+					Game.Rand()
+				end
+			end
+		end
+		return t
+	end
+
+	events.LoadMap = NeedSeed
+
+	local function f(t)
+		local seed = NeedSeed()
+		Game.RandSeed = seed[t.MonsterIndex]
+		t.CallDefault()
+		seed[t.MonsterIndex] = Game.RandSeed
+	end
+
+	events.PickCorpse = f
+	events.CastTelepathy = f
+end
