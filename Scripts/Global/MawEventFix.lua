77db9d1ae45cca370dd41a26b00a7f73da4ed0f9
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2023-07-27 16:01:41 +0200

    Update MawEventFix.lua

--- Scripts/Global/MawEventFix.lua
+++ Scripts/Global/MawEventFix.lua
@@ -19,23 +19,9 @@ end
 
 
 if SETTINGS["TRUENIGHTMARE"]==true then
-	vars.hasPortal={}
-	local function getDistanceToMonster(monster)
-		return math.sqrt((Party.X - monster.X) * (Party.X - monster.X) + (Party.Y - monster.Y) * (Party.Y - monster.Y))
-	end
-
 	function events.Tick()
-		nearbyMonsters=false
-		for monsterIndex = 0, Map.Monsters.high do
-			local monster = Map.Monsters[monsterIndex]
-			local distanceToMonster = getDistanceToMonster(monster)
-			if monster.Id~=121 and monster.Id~=122 and monster.Id~=123 and monster.Id~=133 and monster.Id~=134 and monster.Id~=135 then   
-				if distanceToMonster < 4850 and monster.Active then
-					nearbyMonsters=true
-				end
-			end
-		end 
-		if nearbyMonsters then
+		vars.hasPortal=vars.hasPortal or {}
+		if Party.EnemyDetectorYellow or Party.EnemyDetectorRed then
 			mapvars.event=true
 			for i=0,3 do
 				if Party[i].Spells[31]==true then
