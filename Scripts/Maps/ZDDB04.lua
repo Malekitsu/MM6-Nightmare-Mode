d6ee195b3f2940401d5206c78758d76bf2b808a7
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2023-08-25 19:50:52 +0200

    Update ZDDB04.lua

--- Scripts/Maps/ZDDB04.lua
+++ Scripts/Maps/ZDDB04.lua
@@ -42,7 +42,7 @@ evt.map[100] = function()
 		Game.Time=Game.Time+const.Year*3-Game.Time%const.Day+const.Hour*9
 		local seed = vars.Seed
 		local shrine = vars.shrineBlessings
-		if vars.TRUENIGHTMARE=true then
+		if vars.TRUENIGHTMARE then
 			TRUENIGHTMARE=true
 		end
 		table.clear(vars)
